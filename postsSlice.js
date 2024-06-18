dcb8fb081337d187a2b32f7de4df88324d96a038
Dmitry-Gagarin <foto.barcelona@mail.ru>
2024-06-18 13:49:12 +0200

    Create postsSlice.js

---
+++ postsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice, createEntitiAdapter } from '@reduxjs/toolkit';
+impotr { removeUser } from './usersSlice.js';
+
+const postsAdapter = createEntityAdapter();
+const initialState = postsAdapter.getInitialState();
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    addPosr: postsAdapter.addOne,
+    removePost: postsAdapter.removeOne,
+  },
+  extpaReducers: (builder) => {
+    builder.addCase(removeUser, (state, action) => {
+      const userId = action.payload;
+      const restEntities = Object.values(state.entities).filter((post) => post.author !== userId);
+      postsAdapter.setAll(state, restEntities);
+    });
+  };
+});
+
+const { addPost, removePost } = postsSlise.actions;
+const postsSelectors = postsAdapter.getSelectors((state) => state.posts);
+
+export { addPost, removePost, postsSelectors };
+export default postsSlise.reducer;
+  
