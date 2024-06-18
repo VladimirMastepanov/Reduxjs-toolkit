6fc102f49d0e307eb4b75593d706c4a2978903a9
Vladimir Mastepanov <146621116+VladimirMastepanov@users.noreply.github.com>
2024-06-18 20:20:48 +0200

    Create usersSlice.js

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,19 @@
+import { createSlise, createEtityAdapter } from '@redux.js/toolkit'
+
+const usersAdapter = createAdapter();
+const initialState = usersAdapter.getInitialState();
+
+const usersSlise = createSlise({
+  name: 'users',
+  initialState,
+  redusers: {
+    addUser: userAdapter.addOne,
+    removeUser: userAdapter.removeOne,
+  },
+});
+
+const { addUser, removeUser } = usersSlice.actions;
+
+export { addUser, removeUser };
+export const usersSelectors = userAdapter.getSelectors((state) => state.users);
+export default usersSlice.reducer;
