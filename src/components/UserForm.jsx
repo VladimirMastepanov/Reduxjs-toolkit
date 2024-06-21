42d52c6499676f58cc8d70be92c05dc86e54be14
Vladimir Mastepanov <146621116+VladimirMastepanov@users.noreply.github.com>
2024-06-21 09:25:37 +0200

    Update UserForm.jsx

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -2,3 +2,15 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../features/users/userSlice.js';
 import { nanoid } from '@reduxjs/toolkit';
+
+
+const UserForm = () => {
+  const [ name, setName ] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) = {
+    e.preventDefault();
+    dispatch(addUser({ id: nanoid(), name }));
+    setName('');
+  };
+  
