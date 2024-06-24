e76bb4d19251789b89f0513c0ecc0bba40cafc7b
Vladimir Mastepanov <146621116+VladimirMastepanov@users.noreply.github.com>
2024-06-24 15:06:44 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,3 +4,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store.js';
 import App from './App.jsx';
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+  );
